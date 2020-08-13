16213c704121085033939e33eb52c327766e0e67
zinahue <zinahue@naver.com>
2020-08-13 20:56:01 +0900

    [commit]20200813Thu_FactorialTest.java

---
+++ src/obectOrientedProgramming1/FactorialTest.java
@@ -0,0 +1,21 @@
+package obectOrientedProgramming1;
+
+//예제6-5 271p
+public class FactorialTest {
+
+	public static void main(String[] args) {
+		int result = factorial(4);
+		
+		System.out.println(result);
+	}
+	
+	static int factorial(int n) {
+		int result = 0;
+		if(n == 1) {
+			result = 1;
+		} else {
+			result = n * factorial(n-1);
+		}
+		return result;
+	}
+}
